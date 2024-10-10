a0cc5e3967eacb439a581239d5e85d9bbd1776d8
daothihavy <149304121+daothihavy@users.noreply.github.com>
2024-10-10 10:43:51 +0700

    vyvy

---
+++ lythuyetxml.py
@@ -0,0 +1,30 @@
+# Câu 1: Khái niệm XML
+  # XML (Extensible Markup Language) là ngôn ngữ đánh dấu mở rộng do W3C phát triển, dùng để lưu trữ và mô tả dữ liệu. 
+  # XML cho phép người dùng tự định nghĩa các thẻ tag, giúp truyền và chia sẻ dữ liệu giữa các hệ thống khác nhau một cách linh hoạt.
+
+
+# Câu 2: Khái niệm DOM
+  # DOM (Document Object Model) là mô hình đối tượng tài liệu
+  # Cho phép truy xuất và thao tác nội dung, cấu trúc của tài liệu HTML hoặc XML dưới dạng đối tượng
+  # Nó hỗ trợ các ngôn ngữ lập trình để thao tác tài liệu một cách linh hoạt và có cấu trúc
+
+
+# Câu 3: Cú pháp tài liệu XLM
+  # <nodename>nội dung</nodename>
+    # Trong đó: 
+        #<nodename> là thẻ mở, tên của thẻ này do người dùng tự định nghĩa.
+        #</nodename> là thẻ đóng, tên của thẻ này phải trùng với tên của thẻ mở.
+        #content là nội dung của thẻ này
+
+
+# Câu 4: Khái niệm JSON
+  # JSON (JavaScript Object Notation) là định dạng dữ liệu sử dụng cặp key-value để lưu trữ và truyền dữ liệu
+  #JSON có nguồn gốc từ JavaScript nhưng là định dạng dữ liệu độc lập với ngôn ngữ 
+  # Nhờ định dạng đơn giản, dễ sử dụng và truy vấn, JSON được ứng dụng phổ biến trong việc lưu trữ và trao đổi dữ liệu
+
+
+# Câu 5: Hạn chế của việc triển khai JSON trong Python
+  # Không hỗ trợ các kiểu dữ liệu phức tạp như tuple, set, datetime, và các đối tượng lớp tùy chỉnh
+  # Độ chính xác của số liệu kém, không phân biệt số nguyên và số thực
+  # Hiệu suất hạn chế, khi xử lý một khối lượng lớn dữ liệu, tốc độ xử lý JSON có thể chậm
+
